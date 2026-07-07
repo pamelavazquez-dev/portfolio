a498176e920da2d547136d74fe387cf8d2de1767
Pamela Vazquez <pamevazquez7@gmail.com>
2026-07-07 16:58:01 +0200

    Add CV link to profile

--- src/data/profile.js
+++ src/data/profile.js
@@ -4,7 +4,7 @@ const profile = {
   email: 'pamevazquez7@gmail.com',
   githubUrl: 'https://github.com/pamelavazquez-dev',
   linkedinUrl: 'https://www.linkedin.com/in/pamelavazquez-dev/',
-  cvUrl: '/cv-pamela-vazquez.pdf',
+  cvUrl: 'https://drive.google.com/file/d/1x7-g13SCAyP57wgnE05pURmrC600BGul/view?usp=sharing',
 };
 
 export default profile;
