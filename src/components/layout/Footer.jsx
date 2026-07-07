a20396e813df44b68587d78c443121ce609123c4
Pamela Vazquez <pamevazquez7@gmail.com>
2026-07-07 15:43:11 +0200

    Clean unused portfolio assets

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import profile from '../../data/profile';
 
 function Footer() {
-  return <footer className="footer"> © 2026 {profile.name}. Todos los derechos reservados.</footer>;
+  return <footer className="footer">&copy; 2026 {profile.name}. Todos los derechos reservados.</footer>;
 }
 
 export default Footer;
